36a3ba4701aba6b0fb38c853606e441ba9913cd0
Henry Freel <henryfreel@gmail.com>
2015-08-10 01:54:34 -0400

    new user created

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   
-  root "posts#index"
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
+  root "pages#index"
 
   # users routes
   # get "/signup", to: "users#new"
@@ -14,10 +20,12 @@ Rails.application.routes.draw do
   # blog post routes
   resources :posts
 
+  resources :users, only: [:new, :create, :show]
+
 end
 
 #    Prefix Verb   URI Pattern               Controller#Action
-#      root GET    /                         posts#index
+#      root GET    /                         pages#index
 #     posts GET    /posts(.:format)          posts#index
 #           POST   /posts(.:format)          posts#create
 #  new_post GET    /posts/new(.:format)      posts#new
@@ -26,6 +34,10 @@ end
 #           PATCH  /posts/:id(.:format)      posts#update
 #           PUT    /posts/:id(.:format)      posts#update
 #           DELETE /posts/:id(.:format)      posts#destroy
+#     users POST   /users(.:format)          users#create
+#  new_user GET    /users/new(.:format)      users#new
+#      user GET    /users/:id(.:format)      users#show
+#      DELETE /posts/:id(.:format)      posts#destroy
 
 
 
